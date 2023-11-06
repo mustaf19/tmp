de229000f16e3b6ba3d59f8d6e943c58ad5f71db
mustaf19 <mustafeez0001@gmail.com.com>
2023-11-06 21:20:37 +0530

    added package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+Certainly! Here are 10 points on how to use a computer:
+
+Power On/Off: Start by pressing the power button to turn on the computer and make sure to shut it down properly when you're done to avoid data loss and maintain the computer's health.
+
+Operating System: Familiarize yourself with the computer's operating system, such as Windows, macOS, or Linux, to navigate and manage files and software.
+
+Mouse and Keyboard: Learn how to use the mouse and keyboard for navigation and interaction with the computer. Understand common keyboard shortcuts for efficiency.
+
+Internet Connection: Connect to the internet via Wi-Fi or Ethernet to browse the web, check emails, and access online resources.
+
+Software and Applications: Install and use software and applications for various tasks, such as word processing, web browsing, multimedia, and more.
+
+File Management: Organize and manage your files and folders by creating, deleting, copying, moving, and renaming them.
+
+Security: Protect your computer by using strong passwords, keeping your operating system and software up to date, and using antivirus and firewall software.
+
+Backup: Regularly back up your important files to an external drive or cloud storage to prevent data loss in case of hardware failure or other issues.
+
+Troubleshooting: Learn how to troubleshoot common computer problems, such as frozen applications, slow performance, and connectivity issues.
+
+Customization: Personalize your computer by adjusting settings, wallpapers, and themes to make it more user-friendly and tailored to your preferences.
+
+These points should help you get started with using a computer effectively and safely.
